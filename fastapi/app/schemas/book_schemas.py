b71d0859de3e65b50e5f68f1d21e81ab22feb7dc
rooreckless <rooreckless@gmail.com>
2025-06-25 00:37:31 +0900

    Annotatedで同様に値段フィールドを定義(最低値段だけ100円以上に変えたけど)

--- fastapi/app/schemas/book_schemas.py
+++ fastapi/app/schemas/book_schemas.py
@@ -1,6 +1,7 @@
 # 書籍のデータ構造を定義するスキーマを踏みます
 
 from pydantic import BaseModel,Field
+from typing import Annotated
 
 # 書籍の作成と更新に使うスキーマ
 class BookSchema(BaseModel):
@@ -9,7 +10,8 @@ class BookSchema(BaseModel):
     # カテゴリ
     category: str
     # 値段
-    price: float = Field(..., gt=0,le=5000,)
+    price: Annotated[float, Field(..., ge=100, le=5000)]
+    # price: float = Field(..., gt=0,le=5000,)
 
 # レスポンス用のスキーマ
 # 書籍スキーマを継承してidを含める
